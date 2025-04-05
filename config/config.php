24014fea0d8ef3eb8053b620aa3e27f7ca7a91a5
Gian Carlos Cuello Calle <giancarlos.cuello@gmail.com>
2025-04-05 12:30:53 -0500

    config php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 const URL = 'http://127.0.0.1/php-mvc/';
 const DB_HOST = "http://127.0.0.1/";
-const DB_USER = "root";
-const DB_PASSWORD = "admin23";
-const DB_DATABASE = "gksoftwa_menu_order";
+const DB_USER = "-";
+const DB_PASSWORD = "-";
+const DB_DATABASE = "-";
 const CHARSET = "utf8mb4";
 const PORT = 3306;
 define('VIEWS_PATH', dirname(__DIR__) . '/views/');
